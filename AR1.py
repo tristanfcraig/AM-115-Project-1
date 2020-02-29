5864c35c86e3d91c3f009acb957dfca9dab5901f
Tristan Craig <tcraig@college.harvard.edu>
2020-02-29 16:32:07 -0500

    first commit

---
+++ AR1.py
@@ -0,0 +1,27 @@
+from rcp import get_polls, get_poll_data
+import random
+
+phi = .8
+c = .5 * (1 - phi)
+
+
+noiseeffect = .05 ## Noise will be (-noiseeffect, noiseeffect)
+
+numpolls = 8
+
+finals = []
+
+for k in range(100):
+  X = 0.58
+  for i in range(numpolls):
+    random.seed()
+    r = random.random()
+    noise = (r - 0.5) * (noiseeffect / 2)
+
+    X = c + (phi * X) + noise
+
+  finals.append(X)
+
+print(sum(finals) / len(finals))
+
+
