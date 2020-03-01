68baa9ada6ce16a7521dd3ed953b4da3413658f8
Tristan Craig <tcraig@college.harvard.edu>
2020-03-01 14:26:29 -0500

    csv input

--- AR1.py
+++ AR1.py
@@ -1,30 +1,52 @@
+import csv
+import numpy as np
+import pandas as pd
 import random
 
-# states = []
+states = ['IA','NH','TX']
 # start_date = 
 # end_date = 
 
-phi = .8
-c = .5 * (1 - phi)
+data = pd.read_csv('Data/IA.csv', sep=',')
+rows = data.shape[0]
+
+clinton = data['Clinton']
+sanders = data['Sanders']
+#spread = clinton - sanders
+
+for poll in range(0,rows):
+
+	#if (is_number(clinton[poll])) and (is_number(sanders[poll])): 
+	#	spread = clinton[poll] - spread[poll]
+	
+	print(sanders[poll])
+	#spread.append()
 
 
-noiseeffect = .05 ## Noise will be (-noiseeffect, noiseeffect)
+	
 
-numpolls = 8
 
-finals = []
+#phi = .8
+#c = .5 * (1 - phi)
 
-for k in range(100):
-  X = 0.58
-  for i in range(numpolls):
-    random.seed()
-    r = random.random()
-    noise = (r - 0.5) * (noiseeffect / 2)
 
-    X = c + (phi * X) + noise
+#noiseeffect = .05 ## Noise will be (-noiseeffect, noiseeffect)
 
-  finals.append(X)
+#numpolls = 8
 
-print(sum(finals) / len(finals))
+#finals = []
+
+#for k in range(100):
+#  X = 0.58
+#  for i in range(numpolls):
+#    random.seed()
+#    r = random.random()
+#    noise = (r - 0.5) * (noiseeffect / 2)
+
+    #X = c + (phi * X) + noise
+
+  #finals.append(X)
+
+#print(sum(finals) / len(finals))
 
 
